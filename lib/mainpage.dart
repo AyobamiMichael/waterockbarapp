6f145f23236c85285ef518a8c46a60ddafb38cbc
Ayobami <ayobamiwealth@gmail.com>
2024-09-15 22:51:53 +0100

    update

--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -277,13 +277,16 @@ class SearchDelegateWidget extends SearchDelegate {
                 ),
                 onTap: () {
                   query = filteredBars[index].name;
-                  // showResults(context);
+
                   fetchDataForBar(filteredBars[index].name);
+
                   BarTile.barName = filteredBars[index].name;
                   BarTile.barAddress = MainPageWidgetState.barAddress;
                   BarTile.barImage = MainPageWidgetState.barImage;
                   BarTile.barPhone = MainPageWidgetState.barPhone;
+
                   print(filteredBars[index].name);
+                  showResults(context);
                   Navigator.push(
                       context,
                       MaterialPageRoute(
@@ -302,14 +305,16 @@ class SearchDelegateWidget extends SearchDelegate {
     try {
       // Fetch bar details
       List<Allbars>? allBarsList = await AllBarsFromApi().getAllbars();
+
+      // Find the corresponding bars by name
       var correspondingBars = allBarsList.where((e) => e.barName == barName);
 
-      correspondingBars.forEach((bar) async {
+      for (var bar in correspondingBars) {
         print(bar.barAddress);
         MainPageWidgetState.barAddress = bar.barAddress;
         MainPageWidgetState.barImage = bar.barImage;
         MainPageWidgetState.barPhone = bar.barPhone;
-      });
+      }
     } catch (error) {
       // Handle errors
       print('Error fetching data: $error');
@@ -358,157 +363,3 @@ class SearchingData {
     return bars; // Return the filtered list of bars
   }
 }
-
-
-
-/*class SearchDelegateWidget extends SearchDelegate {
-  final MainPageWidgetState _mainPageWidgetState;
-
-  List<String> newFilterList = [];
-  // List<Bar> bars = [];
-  bool isLoading = true;
-  //final _storage = const FlutterSecureStorage();
-  final SearchingData _searchingData = SearchingData();
-
-  SearchDelegateWidget(this._mainPageWidgetState);
-
-  @override
-  List<Widget> buildActions(BuildContext context) {
-    return [
-      IconButton(
-        icon: const Icon(Icons.clear),
-        onPressed: () {
-          query = '';
-        },
-      ),
-    ];
-  }
-
-  @override
-  Widget buildLeading(BuildContext context) {
-    return IconButton(
-      icon: const Icon(Icons.arrow_back),
-      onPressed: () {
-        close(context, null);
-      },
-    );
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {
-    _searchingData.fetchDataToSearch2(query);
-
-    return ListView.builder(
-        itemCount: _searchingData.bars.length,
-        itemBuilder: (context, index) {
-          return Container(
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.blue),
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            margin: const EdgeInsets.all(8.0),
-            child: ListTile(
-              title: Text(
-                '${_searchingData.bars[index].name} NGN${_searchingData.bars[index].productPrice}',
-                style: const TextStyle(color: Colors.blue),
-              ),
-              onTap: () {
-                //close(context, searchResult[index]);
-                print('okay');
-              },
-              subtitle: Text(_searchingData.bars[index].address),
-            ),
-          );
-        });
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    //fetchDataToSearch();
-    _searchingData.fetchDataToSearch2(query);
-
-    final List<Bar> newSuggestionList =
-        query.isEmpty ? [] : _searchingData.bars.toList();
-
-    List<String>? newSuggestionPriceList =
-        newSuggestionList.map((e) => e.productPrice).toList();
-
-    List<int> intPriceList = [];
-    for (var item in newSuggestionPriceList) {
-      intPriceList.add(int.parse(item));
-    }
-    intPriceList.sort((a, b) => a.compareTo(b));
-    print(newSuggestionList);
-    if (newSuggestionList.isEmpty) {
-      return const Center(
-        child: Text('No suggestions found'),
-      );
-    }
-
-    newSuggestionList.sort((a, b) =>
-        int.parse(a.productPrice).compareTo(int.parse(b.productPrice)));
-
-    return ListView.builder(
-        itemCount: newSuggestionList.length,
-        itemBuilder: (context, index) {
-          return Container(
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.blue),
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              margin: const EdgeInsets.all(8.0),
-              child: ListTile(
-                title: Text(
-                  '${newSuggestionList[index].name} NGN${newSuggestionList[index].productPrice}',
-                  style: const TextStyle(color: Colors.blue),
-                ),
-                onTap: () {
-                  query = newSuggestionList[index].name;
-                },
-                subtitle: Text(newSuggestionList[index].name),
-              ));
-        });
-  }
-}
-
-class SearchingData {
-  List<Bar> bars = [];
-
-  Future<void> fetchDataToSearch2(String query) async {
-    try {
-      // Fetch bar products
-      List<Barproducts>? barProductsList =
-          await BarproductsFromApi().getBarproducts();
-      // Fetch bar details
-      //List<Allbars>? allBarsList = await AllBarsFromApi().getAllbars();
-
-      print(query);
-      // print(barProductsList);
-      List<Barproducts> filteredBarProducts = barProductsList
-          .where((element) => element.catSelected == query)
-          .toList();
-      //   print(filteredBarProducts);
-      for (var barProduct in filteredBarProducts) {
-        //var correspondingBars =
-        //  allBarsList.where((e) => e.barName == barProduct.barName);
-
-        //print(bar.barAddress);
-        print(barProduct.barName);
-        bars.add(Bar(
-          name: barProduct.barName,
-          address: '',
-          productPrice: barProduct.productPrice,
-          phone: '',
-          image: '',
-        ));
-
-        // getAddress();
-      }
-
-      //isLoading = false;
-    } catch (error) {
-      // Handle errors
-      print('Error fetching data: $error');
-    }
-  }
-}*/
